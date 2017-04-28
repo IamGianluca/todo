e81cbc68483f03acd04c0b4b6f8a4ef341a30a3a
Gianluca Rossi <gr.gianlucarossi@gmail.com>
2017-04-28 22:19:22 +0100

    MAINT: refactor models using sqlalchemy orm and create docker test infrastructure (#3)

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,18 @@
 import datetime
+import os
 
 import pytest
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
 
-from todo.task import Task
+from todo.models import Base, Board, State, Task, User
 
+
+TESTDB = 'testdb'
+TESTDB_PATH = os.path.join('5432', TESTDB)
+TESTDB_URI = 'postgresql://docker:changeme@db:' + TESTDB_PATH
+
+# TESTDB_URI = 'postgresql://docker:changeme@db:5432/testdb' #+ TESTDB_PATH
 
 @pytest.fixture(scope='function')
 def today():
@@ -19,13 +28,57 @@ def tomorrow():
 
 @pytest.fixture(scope='function')
 def in7days(today):
-    return today + datetime.timedelta(days=7)
+   return today + datetime.timedelta(days=7)
+
+@pytest.fixture(scope='session')
+def engine():
+    return create_engine(TESTDB_URI)
+
+@pytest.yield_fixture(scope='session')
+def tables(engine):
+    Base.metadata.drop_all(engine)
+    Base.metadata.create_all(engine)
+    yield
+    Base.metadata.drop_all(engine)
+
+@pytest.yield_fixture
+def session(engine, tables):
+    connection = engine.connect()
+    transaction = connection.begin()
+    session = Session(bind=connection)
+
+    yield session
+
+    session.close()
+    transaction.rollback()
+    connection.close()
 
 @pytest.fixture(scope='function')
-def task1(tomorrow):
-    return Task(body="Buy some ham", deadline=tomorrow)
+def user():
+    return User(username='luca', email='my@email.com')
 
 @pytest.fixture(scope='function')
-def task2(tomorrow):
-    return Task(body="Check in for flight to Paris",
-                deadline=tomorrow)
+def another_user():
+    return User(username='another', email='another@email.com')
+
+@pytest.fixture(scope='function')
+def board():
+    return Board(title='board')
+
+@pytest.fixture(scope='function')
+def backlog(board):
+    return State(name='backlog', board=board)
+
+@pytest.fixture(scope='function')
+def hit(board):
+    return State(name='hit', board=board)
+
+@pytest.fixture(scope='function')
+def master(board):
+    return State(name='master', board=board)
+
+@pytest.fixture(scope='function')
+def task(backlog, tomorrow, user, hit):
+    return Task(description='service road bike', assignee=user,
+                state=hit, deadline=tomorrow)
+
